05c828ccd42702a9c054e2909cd85d35d3e26640
Quinn1876 <qhodges0442@gmail.com>
2019-06-07 12:42:47 -0400

    updated pong

--- static/sketches/pong.js
+++ static/sketches/pong.js
@@ -1,9 +1,36 @@
 var w = 600;
 var h = 400;
+var paddle;
+
 
 function setup() {
     createCanvas(w, h).parent('canvasHolder');
+    paddle = Paddle(20); // May need to change this value
 }
 function draw(){
+    clear();
+    fill(0);
+    paddle.draw();
+}
 
+class Paddle{
+    constructor(width) {
+        this.width = width;
+        this.height = 10; // This may need to be changed
+        this.x = width/2;
+        this.y = height - 20;
+        this.XSTEP = 20;
+    }
+
+    draw() {
+        rect(this.x, this.y, this.width, this.height);
+    }
+
+    stepLeft() {
+        this.x -= this.XSTEP;
+    }
+
+    stepRight() {
+        this.x += this.XSTEP;
+    }
 }
